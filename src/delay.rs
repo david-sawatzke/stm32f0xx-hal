6a4f484bd4ce8a21a56fb305613af3fb648d1010
David Sawatzke <david-sawatzke@users.noreply.github.com>
2018-12-21 19:13:02 +0100

    Implement proposed overflow guards and sub MHz compatability

--- src/delay.rs
+++ src/delay.rs
@@ -25,7 +25,7 @@
 //! }
 //! ```
 
-use cast::u32;
+use cast::{u16, u32};
 use cortex_m::peripheral::syst::SystClkSource;
 use cortex_m::peripheral::SYST;
 
@@ -53,20 +53,26 @@ impl Delay {
 }
 
 impl DelayMs<u32> for Delay {
-    fn delay_ms(&mut self, ms: u32) {
-        self.delay_us(ms * 1_000);
+    // At 48 MHz, calling delay_us with ms * 1_000 directly overflows at 0x15D868 (just over the max u16 value)
+    fn delay_ms(&mut self, mut ms: u32) {
+        const MAX_MS: u32 = 0x0000_FFFF;
+        while ms != 0 {
+            let current_ms = if ms <= MAX_MS { ms } else { MAX_MS };
+            self.delay_us(current_ms * 1_000);
+            ms -= current_ms;
+        }
     }
 }
 
 impl DelayMs<u16> for Delay {
     fn delay_ms(&mut self, ms: u16) {
-        self.delay_ms(u32(ms));
+        self.delay_us(ms as u32 * 1_000);
     }
 }
 
 impl DelayMs<u8> for Delay {
     fn delay_ms(&mut self, ms: u8) {
-        self.delay_ms(u32(ms));
+        self.delay_ms(u16(ms));
     }
 }
 
@@ -75,7 +81,11 @@ impl DelayUs<u32> for Delay {
         // The SysTick Reload Value register supports values between 1 and 0x00FFFFFF.
         const MAX_RVR: u32 = 0x00FF_FFFF;
 
-        let mut total_rvr = us * (self.clocks.sysclk().0 / 1_000_000);
+        let mut total_rvr = if self.clocks.sysclk().0 < 1_000_000 {
+            us / (1_000_00 / self.clocks.sysclk().0)
+        } else {
+            us * (self.clocks.sysclk().0 / 1_000_000)
+        };
 
         while total_rvr != 0 {
             let current_rvr = if total_rvr <= MAX_RVR {
